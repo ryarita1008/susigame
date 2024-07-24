22fc654b4e1a1b47f0d52ff68d6ddece2283cf6f
harada <ryarita1008@gmail.com>
2024-07-24 15:52:29 +0900

    指定回数分問題が出題されるよう修正しました。

--- kansei.py
+++ kansei.py
@@ -1,8 +1,9 @@
-
 import random
 
 print("一緒にお皿の色を覚えましょう！")
 print("選択肢｛オレンジ、緑、水色、紫、金黒、赤、金、柄｝")
+
+n = int(input("何問出題しますか？ "))
 
 orange = ["玉子","生げそ","げそ梅","げそマヨ","炙りげそ","なすの浅漬け","いなり寿し","マヨコーン","ツナマヨ","軍艦納豆","きゅうり細巻","梅細巻","しんこ細巻"]
 green = ["びんとろ","えび","いか","甘えび","だし巻玉子","揚げなす","げそ天にぎり","とびこ","ねぎまぐろ","山かけまぐろ","ツナきゅうり細巻"]
@@ -17,51 +18,54 @@ dish = ["オレンジ","緑","水色","紫","黒金","赤","金","花柄"]
 dict = {"オレンジ":orange,"緑":green,"水色":rightblue,"紫":purple,"黒金":blackgold,"赤":red,"金":gold,"花柄":other}
 
 menu = orange + green + rightblue + purple + blackgold + red + gold + other
-quiz = random.choice(menu)
 
-print(quiz)
+i = 0
+while i < n:
+    quiz = random.choice(menu)
+    print(quiz)
 
+    def hantei(a):
+        if ans == dish[a]:
+            print("正解です。")
+            return 1
+        else:
+            print("不正解です。")
+            return 0
 
-def hantei(a):
-    if ans == dish[a]:
-        print("正解です。")
-        return 1
-    else:
-        print("不正解です。")
-        return 0
-
-flag = 0
-while flag == 0:
-    ans = input()
-    if quiz in dict["オレンジ"]:
-        result = hantei(0)
-        if result == 1:
-            flag = 1
-    if quiz in dict["緑"]:
-        result = hantei(1)
-        if result == 1:
-            flag = 1
-    if quiz in dict["水色"]:
-        result = hantei(2)
-        if result == 1:
-            flag = 1
-    if quiz in dict["紫"]:
-        result = hantei(3)
-        if result == 1:
-            flag = 1
-    if quiz in dict["黒金"]:
-        result = hantei(4)
-        if result == 1:
-            flag = 1
-    if quiz in dict["赤"]:
-        result = hantei(5)
-        if result == 1:
-            flag = 1
-    if quiz in dict["金"]:
-        result = hantei(6)
-        if result == 1:
-            flag = 1
-    if quiz in dict["花柄"]:
-        result = hantei(7)
-        if result == 1:
-            flag = 1
+    flag = 0
+    while flag == 0:
+        ans = input()
+        if quiz in dict["オレンジ"]:
+            result = hantei(0)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["緑"]:
+            result = hantei(1)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["水色"]:
+            result = hantei(2)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["紫"]:
+            result = hantei(3)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["黒金"]:
+            result = hantei(4)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["赤"]:
+            result = hantei(5)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["金"]:
+            result = hantei(6)
+            if result == 1:
+                flag = 1
+        elif quiz in dict["花柄"]:
+            result = hantei(7)
+            if result == 1:
+                flag = 1
+        
+    i += 1
